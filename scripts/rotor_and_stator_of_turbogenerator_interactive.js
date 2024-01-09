10e1a6f0e1a84e9fd9ad0336e26de727a3a23fbe
GRATUS <toxa.blr@mail.ru>
2024-01-09 23:57:21 +0300

    v0.0.0.2

--- scripts/rotor_and_stator_of_turbogenerator_interactive.js
+++ scripts/rotor_and_stator_of_turbogenerator_interactive.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     //Обращение к SVG изображению
     const svgObject = document.getElementById('svgObject');
-    
+
     if (svgObject) {
         svgObject.addEventListener('mouseover', function (event) {
             let id = event.target.id
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 svgObject.getElementById('rect' + id).style.opacity = '0.6';
                 svgObject.getElementById('text' + id).style.display = 'inline';
             }
-            else if(id == "video_obmotka"){
+            else if (id == "video_obmotka") {
                 svgObject.getElementById(id).style.cursor = 'pointer';
             }
         });
@@ -24,36 +24,46 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    const modal_video = document.getElementById('myModal');
-    const icon = svgObject.getElementById("video_obmotka");
-    const span = document.getElementsByClassName("close")[0];
-    const video = modal_video.querySelector("iframe");
-
-    icon.onclick = function () {
-        modal_video.style.display = "inline";
-        video.src = "videos/Производство обмоток статоров турбогенераторов и гидрогенераторов.mp4";
-    }
-
-    span.onclick = function () {
-        // Сбрасываем позицию видео на начало
-        video.src = "";
-        modal_video.style.display = "none";
-    }
-
-    window.onclick = function (event) {
-        if (event.target == modal_video) {
-            // Сбрасываем позицию видео на начало
-            video.src = "";
-            modal_video.style.display = "none";
+    function openModal(modalId) {
+        if(modalId == "videoObmotka"){
+            video.src = "videos/Производство обмоток статоров турбогенераторов и гидрогенераторов.mp4";
         }
+        else if(modalId == "obmotkaText"){
+            const modalObject = document.getElementById('obmotkaText');
+            modalObject.querySelector('.modal-content').style.maxWidth = '600px'; /* Ограничение ширины для удобочитаемости */
+            modalObject.querySelector('.modal-content').style.height = 'auto'; /* Автоматическая высота в зависимости от контента */
+            modalObject.querySelector('.modal-content').style.overflowY = 'auto'; 
+        }
+        document.getElementById(modalId).style.display = 'inline';
     }
 
-    const sheet = svgObject.getElementById("sheet");
-    const modal_text = document.getElementById('ModalText');
-    const span_text = document.getElementByClassName("close_text");
-
-    sheet.onclick = function(){
-        modal_text.style.display = 'inline';
+    function closeModal(modalId) {
+        video.src = "";
+        document.getElementById(modalId).style.display = 'none';
     }
+
+    // Назначает обработчик события для каждой кнопки открытия модального окна
+    document.querySelectorAll('.openModalBtn').forEach(function (button) {
+        button.addEventListener('click', function () {
+            var modalId = button.getAttribute('data-modal');
+            openModal(modalId);
+        });
+    });
+
+    // Закрывает модальное окно при клике вне его
+    window.addEventListener('click', function (event) {
+        if (event.target.classList.contains('modal')) {
+            var modalId = event.target.id;
+            closeModal(modalId);
+        }
+    });
+
+    // Назначает обработчик события для каждой кнопки закрытия модального окна
+    document.querySelectorAll('.close').forEach(function (closeButton) {
+        closeButton.addEventListener('click', function () {
+            var modalId = closeButton.closest('.modal').id;
+            closeModal(modalId);
+        });
+    });
 });
 
